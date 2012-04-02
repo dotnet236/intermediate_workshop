c6526437fa7fe4101139a68f31c6f7858c918a88
Anthony Capone <acapone@constantcontact.com>
2012-04-02 11:54:46 -0400

    Added Users Controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Shouter::Application.routes.draw do
   root to: "welcome#index"
   resource :dashboard, only: [:show]
-  resource :shout, only: [:create, :show]
+  resource :shouts, only: [:create, :show]
+  resource :users, only: [:show]
 end
